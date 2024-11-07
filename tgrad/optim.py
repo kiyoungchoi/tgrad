464fba9f7546750ed9bb5d11889d5a033361e503
kiyoungchoi <kaist6123@naver.com>
2024-11-07 09:37:22 +0000

    adding Adam in optim

--- tgrad/optim.py
+++ tgrad/optim.py
@@ -1,10 +1,21 @@
-class SGD:
-  def __init__(self, tensors, lr=0.001):
-    self.tensors = tensors
+class Optimizer:
+  def __init__(self, params, lr):
+    self.params = params
     self.lr = lr
 
+
+class SGD(Optimizer):
   def step(self):
-    for t in self.tensors:
+    for t in self.params:
       t.data -= self.lr * t.grad
     
 
+class Adam(Optimizer):
+  def __init__(self, params, lr, b1=0.9, b2=0.999, eps=1e-8):
+    super(Adam, self).__init__(params, lr)
+    self.b1 = b1
+    self.b2 = b2
+    self.eps = eps
+
+  def step(self):
+    pass
