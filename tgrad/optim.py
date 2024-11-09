a46d825c01aea77f9f387ebdc306b0bb89151163
kiyoungchoi <kaist6123@naver.com>
2024-11-09 03:46:31 +0000

    Adam works

--- tgrad/optim.py
+++ tgrad/optim.py
@@ -22,14 +22,16 @@ class Adam(Optimizer):
     self.b1 = b1
     self.b2 = b2
     self.eps = eps
+    self.t = 0
 
     self.m = [np.zeros_like(t.data) for t in self.params]
     self.v = [np.zeros_like(t.data) for t in self.params]
 
   def step(self):
     for i, t in enumerate(self.params):
+      self.t +=  1
       self.m[i] = self.b1 * self.m[i] + (1 - self.b1) * t.grad
-      self.v[i] = self.b2 * self.v[i] + (1 - self.b2) * t.grad**2
-      self.m[i] /= (1. - self.b1)
-      self.v[i] /= (1. - self.b2)
-      t.data -= (self.lr*self.m[i])/(self.v[i] + self.eps)
+      self.v[i] = self.b2 * self.v[i] + (1 - self.b2) * np.square(t.grad)
+      mhat = self.m[i] / (1. - self.b1**self.t)
+      vhat = self.v[i] / (1. - self.b2**self.t)
+      t.data -= (self.lr * mhat)/(np.sqrt(vhat) + self.eps)
