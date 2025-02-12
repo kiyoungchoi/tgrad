6e3fc1a6e7c5482b41e44f93b0e68b06b7074251
kiyoungchoi <kaist6123@naver.com>
2025-02-12 01:13:29 +0000

    add type check

--- tgrad/tensor.py
+++ tgrad/tensor.py
@@ -1,26 +1,27 @@
+from typing import Union, Type, List, Tuple, Callable, Any, Optional
 from functools import partialmethod
 import numpy as np
 
 
 # *** start with two base classes ***
 class Tensor:
-    def __init__(self, data):
+    def __init__(self, data: np.ndarray) -> None:
         # print(data.shape, data)
-        if type(data) != np.ndarray:
+        if not isinstance(data, np.ndarray):
             print(f"error constructing tensor with {data} ")
-            assert(False)
+            assert False
         if data.dtype == np.float64:
             print(f"are you sure you want float64 in {data}?")
-        self.data = data
-        self.grad = None
+        self.data: np.ndarray = data
+        self.grad: Optional[np.ndarray] = None
     
         # internal variables used for autograd graph construnction 
-        self._ctx = None
+        self._ctx: Optional["Function"] = None
     
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Tensor {self.data} with grad {self.grad}"
     
-    def backward(self, allow_fill=True):
+    def backward(self, allow_fill: bool = True) -> None:
         # # 텐서 생성 example
         # a = Tensor(np.array([2.0]))
         # b = Tensor(np.array([3.0]))
@@ -59,22 +60,23 @@ class Tensor:
             if g is None:
                 continue
             if g.shape != t.data.shape:
-                print("grad shape must match tensor shape in {se.f_ctx}, {g.shape} != {t.data.shape}")
+                print(f"grad shape must match tensor shape in {self._ctx}, {g.shape} != {t.data.shape}")
                 assert(False)
             t.grad = g
             t.backward(False)
     
-    def mean(self):
+    def mean(self) -> "Tensor":
         div = Tensor(np.array([1/self.data.size], dtype=self.data.dtype))
         return self.sum().mul(div)
     
 # An instaniation of the Function is the Context
+
 class Function:
-    def __init__(self, *tensors):
-        self.parents = tensors
-        self.saved_tensors = []
+    def __init__(self, *tensors: "Tensor") -> None:
+        self.parents: Tuple[Tensor, ...] = tensors
+        self.saved_tensors: List[Any] = []
     
-    def save_for_backward(self, *x):
+    def save_for_backward(self, *x: Any) -> None:
         # # extend() 사용 예제
         # list1 = [1, 2, 3]
         # list2 = [4, 5, 6]
@@ -85,13 +87,13 @@ class Function:
         self.saved_tensors.extend(x)
 
     # note that due to how partialmethod works, self and arg are switched
-    def apply(self, arg, *x):
+    def apply(self, arg: Union["Tensor", Type["Function"]], *x: "Tensor") -> "Tensor":
         # ctx = arg(self, *x)
         # ret = Tensor(arg.forward(ctx, self.data, *[t.data for t in x]))
         # ret._ctx = ctx
         # return ret
         # print(type(arg) == Tensor) # to test
-        if type(arg) == Tensor:
+        if isinstance(arg, Tensor):
             op = self
             x = [arg]+list(x)
         else: 
@@ -102,35 +104,39 @@ class Function:
         ret._ctx = ctx
         return ret
 
-def register(name, fxn):
+def register(name: str, fxn: Type[Function]) -> None:
     setattr(Tensor, name, partialmethod(fxn.apply, fxn))
 
 class Dot(Function):
     @staticmethod
-    def forward(ctx, input, weight):
+    def forward(ctx: Function, input: np.ndarray, weight: np.ndarray) -> np.ndarray:
         # input is with a lot of 0's
         # weight is with random.uniform()
         ctx.save_for_backward(input, weight)
         return input.dot(weight)
     
     @staticmethod
-    def backward(ctx, grad_output):
+    def backward(ctx: Function, grad_output: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         input, weight = ctx.saved_tensors
         grad_input = grad_output.dot(weight.T)
         grad_weight = grad_output.T.dot(input).T
         return grad_input, grad_weight
+
+# def dot(self, other: "Tensor") -> "Tensor":
+#     # 내부적으로 Dot.apply를 호출하며, 이때 self.data와 other.data를 전달합니다.
+#     return self.dot_fn.apply(self, other)
 register('dot', Dot)
 
 class ReLU(Function):
     @staticmethod
-    def forward(ctx, input):
+    def forward(ctx: Function, input: np.ndarray) -> np.ndarray:
         # 순전파: 입력값을 저장하고 ReLU 연산 수행
         # it is not layer, just act for input 
         ctx.save_for_backward(input)
         return np.maximum(input, 0)
     
     @staticmethod
-    def backward(ctx, grad_output):
+    def backward(ctx: Function, grad_output: np.ndarray) -> np.ndarray:
         # 역전파: 저장된 입력값을 사용하여 기울기 계산
         input, = ctx.saved_tensors
         grad_input = grad_output.copy()
@@ -140,8 +146,8 @@ register('relu',ReLU)
 
 class LogSoftmax(Function):
     @staticmethod
-    def forward(ctx, input):
-        def logsumexp(x):
+    def forward(ctx: Function, input: np.ndarray) -> np.ndarray:
+        def logsumexp(x: np.ndarray) -> np.ndarray:
             c = x.max(axis=1)
             # 예시: 입력값이 [300, 200, 500]일 때
             # 1. c = 500 (최대값)
@@ -155,7 +161,7 @@ class LogSoftmax(Function):
         return output 
     
     @staticmethod
-    def backward(ctx, grad_output):
+    def backward(ctx: Function, grad_output: np.ndarray) -> np.ndarray:
         output, = ctx.saved_tensors
         return grad_output - np.exp(output)*grad_output.sum(axis=1).reshape(-1, 1)
 
@@ -163,12 +169,12 @@ register('logsoftmax', LogSoftmax)
 
 class Mul(Function):
     @staticmethod
-    def forward(ctx, x, y):
+    def forward(ctx: Function, x: np.ndarray, y: np.ndarray) -> np.ndarray:
         ctx.save_for_backward(x, y)
         return x*y
     
     @staticmethod
-    def backward(ctx, grad_output):
+    def backward(ctx: Function, grad_output: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         x, y = ctx.saved_tensors
         return y*grad_output, x*grad_output
 
@@ -183,12 +189,12 @@ class Sum(Function):
     # print(a.grad)  # 출력: 1.0
     # print(b.grad)  # 출력: 1.0
     @staticmethod
-    def forward(ctx, input):
+    def forward(ctx: Function, input: np.ndarray) -> np.ndarray:
         ctx.save_for_backward(input)
         return np.array([input.sum()])
     
     @staticmethod
-    def backward(ctx, grad_output):
+    def backward(ctx: Function, grad_output: np.ndarray) -> np.ndarray:
         input, = ctx.saved_tensors
         return grad_output * np.ones_like(input)
 register('sum', Sum)
@@ -196,11 +202,11 @@ register('sum', Sum)
 
 class Add(Function):
     @staticmethod
-    def forward(ctx, x, y):
+    def forward(ctx: Function, x: np.ndarray, y: np.ndarray) -> np.ndarray:
         return x+y
 
     @staticmethod 
-    def backward(ctx, grad_output):
+    def backward(ctx: Function, grad_output: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         return grad_output, grad_output
 register('add', Add)
 
@@ -217,7 +223,7 @@ except ImportError:
 class Conv2D(Function):
     @staticmethod
     @jit
-    def inner_forward(x, w):
+    def inner_forward(x: np.ndarray, w: np.ndarray) -> np.ndarray:
         cout, cin, H, W = w.shape
         ret = np.zeros((x.shape[0], cout, x.shape[2]-(H-1), x.shape[3]-(W-1)), dtype=w.dtype)
         for j in range(H):
@@ -230,7 +236,7 @@ class Conv2D(Function):
 
     @staticmethod
     @jit
-    def inner_backward(grad_output, x, w):
+    def inner_backward(grad_output: np.ndarray, x: np.ndarray, w: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         dx = np.zeros_like(x) # format 
         dw = np.zeros_like(w) # format
         cout, cin, H, W = w.shape  #filter
@@ -246,23 +252,23 @@ class Conv2D(Function):
         return dx, dw
 
     @staticmethod
-    def forward(ctx, x, w):
+    def forward(ctx: Function, x: np.ndarray, w: np.ndarray) -> np.ndarray:
         ctx.save_for_backward(x, w)
         return Conv2D.inner_forward(x, w)
     @staticmethod
-    def backward(ctx, grad_output):
+    def backward(ctx: Function, grad_output: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         return Conv2D.inner_backward(grad_output, *ctx.saved_tensors)
 
 register('conv2d', Conv2D)
 
 class Reshape(Function):
     @staticmethod
-    def forward(ctx, x, shape):
+    def forward(ctx: Function, x: np.ndarray, shape: Tuple[int, ...]) -> np.ndarray:
         ctx.save_for_backward(x.shape)
         return x.reshape(shape)
     
     @staticmethod
-    def backward(ctx, grad_output):
+    def backward(ctx: Function, grad_output: np.ndarray) -> Tuple[np.ndarray, Optional[Any]]:
         in_shape,  = ctx.saved_tensors
         return grad_output.reshape(in_shape), None 
 register('reshape', Reshape)
